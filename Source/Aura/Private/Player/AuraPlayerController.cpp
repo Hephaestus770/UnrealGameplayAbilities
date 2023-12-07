dcee5fb648a6f30158bcdbb1b8cae2397e7529d1
Hephaestus770 <m.eneserdogan@gmail.com>
2023-12-08 02:01:01 +0300

    Highlighting Enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,12 +4,90 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
+
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
 
 }
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	
+	CursorTrace();
+
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/**
+	* 1. Both actors are null
+	*	- Do nothing
+	* 2. LastActor is null, ThisActor is valid 
+	*	- Highlight ThisActor
+	* 3. LastActor is valid, ThisActor is null 
+	*	- Unlight LastActor
+	* 4. Both actors are valid 
+	*	- Unlight LastActor, Highlight ThisActor
+	* 5. Both are valid and are the same actor 
+	*	- Do nothing
+	*/
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// 2.Case
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Both are null, do nothing
+		}
+		
+	}
+
+	else // LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			// 3.Case
+			LastActor->UnHighlightActor();
+
+		}
+		else // Both are valid Case 4 and 5
+		{
+			if (LastActor != ThisActor) 
+			{
+				// 4. Case 
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+
+			}
+
+			else
+			{
+				// Case 5, do nothing
+
+			}
+		}
+	}
+
+
+
+}
+
 
 void AAuraPlayerController::BeginPlay()
 {
